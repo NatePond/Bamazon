a21ff635dc2972fbc29da528d12a6df475b60010
Nate Pond <nathaniel.p.pond@gmail.com>
2017-05-10 23:33:01 -0400

    video commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,40 +13,38 @@ var connection = mysql.createConnection({
   database: "Bamazondb"
 });
 
-function bamazonify(){
-inquirer.prompt([{
+function bamazonify() {
+  inquirer.prompt([{
     name: "position",
     type: "input",
     message: "What is the Item ID?",
-  },
-  {
+  }, {
     name: "Quantity",
     type: "input",
     message: "How Many?",
-  }]
-  ).then(function(answer) {
+  }]).then(function(answer) {
 
     var query = "SELECT position, price, stock_quantity FROM stock WHERE ?";
 
-    connection.query(query, { position: answer.position}, function(err, res) {
-          var stock_init = res[0].stock_quantity;
-          var stock_new = stock_init - answer.Quantity;
-          var mult = answer.Quantity;
-          var price = mult*res[0].price;
-         if(stock_init >= answer.Quantity){
-          connection.query("UPDATE stock SET ? WHERE ?", [{
-            stock_quantity: stock_new}, 
-            {position: answer.position}], 
-            function(err, res){
-             console.log("Order Placed!")
-             console.log("You just spent: $" + price + "!")
-            });
-        }
-        return console.log("Order too large!")
-        }); 
-         // return (console.log("Order too large"))
-
-});
+    connection.query(query, { position: answer.position }, function(err, res) {
+      var stock_init = res[0].stock_quantity;
+      var stock_new = stock_init - answer.Quantity;
+      var mult = answer.Quantity;
+      var price = mult * res[0].price;
+      if (stock_init >= answer.Quantity) {
+        connection.query("UPDATE stock SET ? WHERE ?", [{
+              stock_quantity: stock_new
+            },
+            { position: answer.position }
+          ],
+          function(err, res) {
+            console.log("Order Placed!")
+            console.log("You just spent: $" + price + "!")
+          });
+      }
+      return console.log("Order too large!")
+    });
+  });
 };
 
 
